1fff2f22a4701303930b2e592d46aee8d4e471dc
Maximiliano Potje Geraldo <maximiliano.geraldo@gmail.com>
2021-09-16 23:45:15 -0300

    Adicionando endpoint REST

--- src/main/java/com/maximiliano/curso/services/CategoriaService.java
+++ src/main/java/com/maximiliano/curso/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.maximiliano.curso.domain.Categoria;
 import com.maximiliano.curso.repositories.CategoriaRepository;
+import com.maximiliano.curso.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,8 +18,9 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
 		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 
