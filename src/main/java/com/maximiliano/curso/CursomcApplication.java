d0157d0d0a4ed6ea59534004c64f0261a77dd601
Maximiliano Potje Geraldo <maximiliano.geraldo@gmail.com>
2021-09-20 23:36:09 -0300

    Adicionando Cidade e Estado

--- src/main/java/com/maximiliano/curso/CursomcApplication.java
+++ src/main/java/com/maximiliano/curso/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.maximiliano.curso.domain.Categoria;
+import com.maximiliano.curso.domain.Cidade;
+import com.maximiliano.curso.domain.Estado;
 import com.maximiliano.curso.domain.Produto;
 import com.maximiliano.curso.repositories.CategoriaRepository;
+import com.maximiliano.curso.repositories.CidadeRepository;
+import com.maximiliano.curso.repositories.EstadoRepository;
 import com.maximiliano.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,7 +23,13 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	private ProdutoRepository produtoRepository;  
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -46,6 +56,21 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 =  new Estado(null, "Minas Gerais");
+		Estado est2 =  new Estado(null, "Sao Paulo");
+		
+		Cidade cid1 = new Cidade(null,"Uberlândia",est1);
+		Cidade cid2 = new Cidade(null,"São Paulo",est2);
+		Cidade cid3 = new Cidade(null,"Campinas",est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
+		
+		
 		
 		
 	}
---
+++ src/main/java/com/maximiliano/curso/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.maximiliano.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maximiliano.curso.domain.Cidade;
+
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
